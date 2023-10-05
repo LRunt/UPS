1937b298adce8884c133787899d1d738bd2823b2
Lukas Runt <lukas.runt@seznam.cz>
2023-10-05 09:54:26 +0200

    Reversing string without using strrev()

--- Bonus_exercises/reverse_client.c
+++ Bonus_exercises/reverse_client.c
@@ -3,6 +3,16 @@
 
 #define MAX_BUFFER_SIZE 255
 
+void reverseString(char* string){
+    int length = strlen(string);
+
+    for(int i = 0; i < length /2; i++){
+        char temp = string[i];
+        string[i] = string[length - (i + 1)];
+        string[length - (i +1)] = temp;
+    }
+}
+
 int main(void){
 
     char str1[MAX_BUFFER_SIZE];
@@ -10,9 +20,9 @@ int main(void){
     printf("Enter a string:\n");
     fgets(str1, sizeof(str1), stdin);
 
-    strrev(str1);
+    reverseString(str1);
 
-    printf("%s\n", str1);
+    printf("Reversed string: %s", str1);
 
     return 0;
 }
